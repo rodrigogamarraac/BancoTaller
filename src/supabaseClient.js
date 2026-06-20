197dd384528fa126e1c50b29b50b6a2f223d5507
Rodrigo Gamarra <rodrigo007g987@gmail.com>
2026-06-20 00:41:19 -0400

    Arreglando link de conexion con supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = "https://qvjllnlockwwvbfdpcgl.supabase.co"
-const supabaseKey = "sb_publishable_GzjmWQ3T5w2Sfm8Ytiu4Og_bOkOooEb"
+const supabaseUrl = "https://giutujnkwdtnpvksgrni.supabase.co"
+const supabaseKey = "sb_publishable_GzpdvC5aEduVeH6O0HQo4A_AxKlknu_"
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
